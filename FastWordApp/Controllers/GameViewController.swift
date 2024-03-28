a6d991751dd339d89a1dc579b86eff444013d0a3
Ina Burström <inaburstrom@hotmail.com>
2024-03-28 14:12:41 +0100

    Added outlets to all components in GameViewController.

--- FastWordApp/Controllers/GameViewController.swift
+++ FastWordApp/Controllers/GameViewController.swift
@@ -9,21 +9,14 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var writeWordTextfield: UITextField!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var writeThisWordLabel: UILabel!
+    @IBOutlet weak var pointsCounterLabel: UILabel!
+    @IBOutlet weak var wordCounterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
