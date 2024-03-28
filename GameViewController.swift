e444629597a9e28eb570ba8271b732675736f19a
Ina Burström <inaburstrom@hotmail.com>
2024-03-28 12:02:07 +0100

    Added components for game view.

---
+++ GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  FastWordApp
+//
+//  Created by Ina Burström on 2024-03-28.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
