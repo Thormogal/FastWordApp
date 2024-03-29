bcd57b5d759ccbefd2b7fdc95a5b39fbf53c5673
Ina Burström <inaburstrom@hotmail.com>
2024-03-29 10:13:20 +0100

    Added wordmanager model.

---
+++ FastWordApp/Models/WordManager.swift
@@ -0,0 +1,15 @@
+//
+//  WordManager.swift
+//  FastWordApp
+//
+//  Created by Ina Burström on 2024-03-29.
+//
+
+import Foundation
+
+class WordManager {
+    
+    var originalWords = ["pineapple","strawberry","lingonberry","passion fruit","apple","pear","kiwi","orange","watermelon","cape gooseberry"]
+    var currentWords: [String]
+    
+}
